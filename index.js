ac492db2a9bee062923b5ae6254d9c91a87c9236
SaintOfAges <30766543+SaintOfAges@users.noreply.github.com>
2018-03-10 20:29:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+    client.user.setActivity('With Your Soul ;-)', {type: 'Playing'});
 });
 
 client.on('message', msg => {
